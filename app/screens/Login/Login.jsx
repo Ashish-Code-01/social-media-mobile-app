9edc09623bcdc54f89cc770f05f816204c824327
Code-With-Aashish <tanujgaming87@gmail.com>
2024-11-18 12:08:09 +0530

    adding the login page

--- app/screens/Login/Login.jsx
+++ app/screens/Login/Login.jsx
@@ -1,14 +1,96 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.text}>Login</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="black"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="black"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#262626',
+  },
+  form: {
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    width: '80%',
+    borderRadius: 25,
+    padding: 20,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 20,
+  },
+  input: {
+    height: 50,
+    width: '100%',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    paddingHorizontal: 30,
+    marginBottom: 30,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  button: {
+    height: 50,
+    width: '70%',
+    backgroundColor: 'black',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    shadowColor: 'white',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5, 
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
