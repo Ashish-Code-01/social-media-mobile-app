204149b312a80be635302e4432a89963eaf1b2bf
Code-With-Aashish <tanujgaming87@gmail.com>
2024-11-28 17:51:23 +0530

    adding the loader in the app

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Animated, StyleSheet } from 'react-native';
 
 const Loader = () => {
+    const animation = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        // Create the bouncing animation using an infinite loop
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(animation, {
+                    toValue: 1,
+                    duration: 1000,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(animation, {
+                    toValue: 0,
+                    duration: 1000,
+                    useNativeDriver: true,
+                }),
+            ])
+        ).start();
+    }, [animation]);
+
+    // Interpolating the animation to get the box-shadow-like effect
+    const translateX = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [-38, 38],
+    });
+
+    const translateY = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [-12, 12],
+    });
+
     return (
         <View style={styles.container}>
-            <ActivityIndicator size="large" color="#0000ff" />
+            <Animated.View
+                style={[
+                    styles.loader,
+                    {
+                        transform: [
+                            { translateX },
+                            { translateY },
+                        ],
+                    },
+                ]}
+            />
         </View>
     );
 };
@@ -14,7 +55,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f5fcff',
+    },
+    loader: {
+        width: 12,
+        height: 12,
+        borderRadius: 50,
+        backgroundColor: '#FFF',
+        margin: 15,
     },
 });
 
